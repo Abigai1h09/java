b2c1aff2a91347af7550a40139ec39f0838b6695
Abigai1h09 <abigailh49@nycstudents.net>
2025-09-17 23:25:33 +0000

    activity 4

---
+++ Java-4-Activity/Main.java
@@ -0,0 +1,82 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+ void init(){
+// Define a variable for each situation in the challenges below. Assign a value to each variable.
+// Display the value of each variable to the console.
+
+/* Challenge 1:    
+   1) student's age 
+    2) number of people that live with you
+    3) number of items in a cart 
+*/
+ int student_age = 15;
+    System.out.println(student_age);
+    System.out.println("Student's age is " + student_age);
+
+int num0fppl = 2;
+    System.out.println(num0fppl);
+    System.out.println("number of people that live with you " + num0fppl);
+
+int num0fitems = 4;
+    System.out.println(num0fitems);
+    System.out.println("number of items in a cart " + num0fitems);
+
+/*  Challenge 2:
+    1) temperature 
+    2) student's GPA
+    3) growth rate of your investment
+*/
+float temp = 62;
+    System.out.println(temp);
+    System.out.println("temperature is " + temp);
+
+double studentGpa = 96.9;
+    System.out.println(studentGpa);
+    System.out.println("student's Gpa is " + studentGpa);
+
+double growthRate = 5.5;
+    System.out.println(growthRate);
+    System.out.println("growth rate of your investment is " + growthRate);
+
+/*  Challenge 3:
+    1) Student's last name 
+    2) student's first initial
+    3) description of an item 
+*/
+ String studentlastName = "Ho";
+    System.out.println(studentlastName);
+    System.out.println("student's last name is " + studentlastName);
+
+char studentfirstInitial = 'A';
+    System.out.println(studentfirstInitial);
+    System.out.println("student's first initial is " + studentfirstInitial);
+
+String item = "perfume";
+    System.out.println(item);
+    System.out.println("description of an item " + item);
+
+/*  Challenge 4:
+    1) the status whether lights are on
+    2) the status whether the game is over
+    3) the status whether the battery is charging
+*/
+boolean islightsOn = false;
+    System.out.println(islightsOn);
+    System.out.println("are the lights on " + islightsOn);
+
+boolean isgameOver = true;
+    System.out.println(isgameOver);
+    System.out.println("is the game over " + isgameOver);
+
+boolean battery = true;
+    System.out.println(battery);
+    System.out.println("is the battery charging " + battery);
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
