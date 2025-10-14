1e9dc3bf69b35d19994d66d60df42c47e92097bd
Abigai1h09 <abigailh49@nycstudents.net>
2025-10-14 16:14:53 +0000

    task 10

--- Java-8-Activity/Main.java
+++ Java-8-Activity/Main.java
@@ -16,8 +16,8 @@ class Main {
    }
   //3: Sphere volume
    double sphereVolume(double radius){
-    return 4/3.*Math.PI*Math.pow(radius,3);
-    
+    double volume= 4/3.* Math.PI* Math.pow(radius,3);
+    return volume;
   }
   
   //4: Cone volume
@@ -50,18 +50,22 @@ class Main {
      System.out.println("Temp in deg C is " + FtoC(F));  
     
     //3
-  System.out.println("Enter the radius of the sphere ");
-    double rad = Input.readDouble();
-    print("The volume of a sphere with a radius of "+rad+" has a volume of "+sphereVolume(rad));
+  System.out.println("Enter radius of the sphere");
+    double radius = Input.readDouble();
+    double volume = sphereVolume();
+    System.out.println("Volume of the Sphere is " + volume);
+
 
     //4
   System.out.println("Enter the radius of the cone ");
     rad = Input.readDouble();
+
     System.out.println("Enter the height of the cone ");
     double height = Input.readDouble();
-    print("The volume of a cone with a radius of "+rad+
-          " and height of "+height+
-          " has a volume of "+coneVolume(rad,height));
+
+   volume = coneVolume(radius, height);
+   System.out.println("The volume of the cone is " + volume);
+   
     //5
     System.out.println("Enter x1 ");
     double x1 = Input.readDouble();
@@ -72,8 +76,8 @@ class Main {
     System.out.println("Enter y2 ");
     double y2 = Input.readDouble();
 
-    print("the distance between point ("+x1+","+y1+") and ("+
-          x2+","+y2+") is "+distance(x1,x2,y1,y2));                 
+    double result = distance(x1, y1, x2, y2);
+   System.out.println("The distance between two points is " + result);                
         
   }
   
